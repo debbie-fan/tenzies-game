b2fa9e5c1ebc6308789f0181b3cb0a81113b0cba
Debbie Fan <debbiehmfan@outlook.com>
2023-01-01 21:11:57 -0500

    Add button to reroll all dice on click

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
+import React from 'react';
 import Die from './components/Die';
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice[i] = Math.ceil(Math.random() * 6)
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map(die => <Die value={die}/>)
+
   return (
     <main>
       <div className='dice-container'>
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
+        {diceElements}
       </div>
+      <button 
+        className='button-roll'
+        onClick={rollDice}
+      >
+          Roll
+      </button>
     </main>
   );
 }
